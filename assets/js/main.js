3a3ae61cd80a099f202447939f953bb34eee5021
henriklindstad <henrik.haugen.lindstad@gmail.com>
2023-09-11 08:51:42 +0200

    #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
-$('.vote-card').on('click', function () {
-  $(this).css('background', $(this).css('background'));
-  $(this).css('color', $(this).css('color'));
-  $(this).find('.hint-img').css('filter', $(this).find('.hint-img').css('filter'));
+let selected = '';
 
-  $('#vote-button').prop('disabled', false);
-  $('#vote-button').css('background', $(this).css('background'));
-  $('#vote-button').text('Stem' + $(this).text());
+$('.vote-card').on('click', function () {
+  if (!selected && $(this).text() != selected) {
+    selected = $(this).text();
+
+    $(this).css('background', $(this).css('background'));
+    $(this).css('color', $(this).css('color'));
+    $(this).find('.hint-img').css('filter', $(this).find('.hint-img').css('filter'));
+
+    $('#vote-button').prop('disabled', false);
+    $('#vote-button').css('background', $(this).css('background'));
+    $('#vote-button').text('Stem' + $(this).text());
+  } else if ($(this).text() == selected) {
+    selected = '';
+
+    $(this).css('background', '');
+    $(this).css('color', '');
+    $(this).find('.hint-img').css('filter', '');
+
+    $('#vote-button').prop('disabled', true);
+    $('#vote-button').css('background', '');
+    $('#vote-button').text('Velg et parti');
+  }
+});
+
+$('#vote-button').on('click', function () {
+  alert('Du har stemt ' + selected.trim());
 });

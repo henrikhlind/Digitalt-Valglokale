aee7d81a579e2c61f8c14a3df9eefacd17ef64d8
henriklindstad <henrik.haugen.lindstad@gmail.com>
2023-09-19 19:00:21 +0200

    #12

--- public/script/services/vote.js
+++ public/script/services/vote.js
@@ -1,11 +1,6 @@
 $('#vote-button').on('click', function () {
-  fetch(`/increment-vote/${$(this).attr('dbid')}`)
-    .then((response) => response.text())
-    .then((data) => {
-      console.log(data); // Output: "Incrementing data for ID: 123"
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  fetch(`/increment-vote/${$(this).attr('dbid')}`).catch((error) => {
+    console.error(error);
+  });
   window.location.href = '/confirmed';
 });

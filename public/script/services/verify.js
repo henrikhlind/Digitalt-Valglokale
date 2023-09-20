0436df60a022df4792bea80160db65972628f727
henriklindstad <henrik.haugen.lindstad@gmail.com>
2023-09-20 15:40:25 +0200

    #13

--- public/script/services/verify.js
+++ public/script/services/verify.js
@@ -1,14 +1,7 @@
-$('#name-input').on('keyup', function (e) {
-  if ($(this).val()) {
+$('#phone-input').on('keyup', function (e) {
+  if (window.libphonenumber.isValidNumber($(this).val(), 'NO')) {
     $('#verify-button').prop('disabled', false);
   } else {
     $('#verify-button').prop('disabled', true);
-  }
-});
-
-$('#verify-button').on('click', function () {
-  if ($('#name-input').val()) {
-    localStorage.setItem('name', $('#name-input').val());
-    window.location = '/vote';
   }
 });
